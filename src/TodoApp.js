44bd535e86ae9aa736a1fecf5fedf4071c0ce111
joannaylin <lin.joanna.y@gmail.com>
2020-10-12 21:25:00 -0500

    added localstorage, and refactored to custom hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import useTodoState from "./hooks/useTodoState"
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
@@ -6,38 +7,14 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import TodoList from "./TodoList.js";
 import TodoForm from "./TodoForm.js";
-import { v4 as uuidv4 } from "uuid";
 
 export default function TodoApp() {
-  const initialTodos = [
-    { id: uuidv4(), task: "study algos", completed: true },
-    { id: uuidv4(), task: "apply to jobs", completed: false },
-    { id: uuidv4(), task: "sleep", completed: false },
-  ];
-  const [todos, setTodos] = useState(initialTodos);
-
-  const addTodo = (newTodoText) => {
-    setTodos([...todos, { id: uuidv4(), task: newTodoText, completed: false }]);
-  };
-
-  const removeTodo = (todoId) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
-    setTodos(updatedTodos);
-  };
-
-  const toggleTodo = (todoId) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
-  };
-
-  const editTodo = (todoId, newTask) => {
-    const updatedTodos = todos.map((todo) => 
-      todo.id === todoId ? {...todo, task: newTask }  : todo
-    )
-    setTodos(updatedTodos)
-  }
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]")
+  const {todos, addTodo, removeTodo, toggleTodo, editTodo} =useTodoState(initialTodos);
+  
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <Paper
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,24 +5,24 @@ import Divider from "@material-ui/core/Divider";
 import Todo from "./Todo";
 
 export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-  return (
-    <Paper>
-      <List>
-        {todos.map((todo,i) => (
-          <>
-            <Todo
-              id={todo.id}
-              task={todo.task}
-              key={todo.id}
-              completed={todo.completed}
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              editTodo={editTodo}
-            />
-            {i < todos.length - 1 && <Divider />}
-          </>
-        ))}
-      </List>
-    </Paper>
-  );
+  if (todos.length)
+    return (
+      <Paper>
+        <List>
+          {todos.map((todo, i) => (
+            <>
+              <Todo
+              {...todo}
+                key={todo.id}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                editTodo={editTodo}
+              />
+              {i < todos.length - 1 && <Divider />}
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  return null;
 }
